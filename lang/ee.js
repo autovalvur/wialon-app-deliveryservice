b1039e9788ddf04033e1225ef4908733327d351f
autovalvur <siim@carcops.ee>
2015-02-26 11:17:35 +0200

    Eesti keel

---
+++ lang/ee.js
@@ -0,0 +1,138 @@
+TRANSLATIONS = {
+	"Add addresses of delivery and indicate time range to visit them.": "",
+
+	"Add address": "",
+
+	"Delivery address:": "",
+
+	"Deliver from:&nbsp;": "",
+
+	"&nbsp;to:&nbsp;": "",
+
+    "Mon": "",
+
+    "Tue": "",
+
+    "Wed": "",
+
+    "Thu": "",
+
+    "Fri": "",
+
+    "Sat": "",
+
+    "Sun": "",
+
+	"h": "",
+
+	"m": "",
+
+	"weeks": "",
+	
+	"Stay duration, min:&nbsp;": "",
+
+	"Speed, km/h:": "",
+
+	"Lock first address": "",
+
+	"Lock last address": "",
+
+	"Stick to schedule": "",
+
+	"Snap to roads": "",
+
+	"Routing provider:": "",
+
+	"Calculate": "",
+
+	"Show on map": "",
+
+	"Print": "",
+
+	"Save in Wialon": "",
+	
+	"Address": "",
+
+	"Time": "",
+
+	"Interval": "", 
+
+	"Distance": "",
+	
+	"Total:": "",
+
+	"Unable to connect to the server.": "",
+	
+	"Error when working with Yandex Maps.": "",
+
+	"Unable to calculate the route: check input data.": "",
+
+	"Unable to optimize the route: not enough time to visit all points. Required:": "",
+
+	"Unable to calculate the route: no roads available.": "",
+
+	"Progress:": "",
+
+	"Route": "",
+
+	"Unable to save the route to Wialon.": "",
+
+	"Optimized schedule": "",
+
+	"Route saved to Wialon.": "",
+
+	"Delivery service": "",
+
+	"Add from map": "",
+
+	"Add from geofences": "",
+
+	"Add from POIs": "",
+
+	"Delete address": "",
+
+	"Save": "",
+
+    "ft": "",
+
+    "km/h": "",
+
+    "mph": "",
+
+    "km": "",
+
+    "mi": "",
+
+    "Sunday": "",
+    "Monday": "",
+    "Tuesday": "",
+    "Wednesday": "",
+    "Thursday": "",
+    "Friday": "",
+    "Saturday": "",
+
+    "January": "",
+    "February": "",
+    "March": "",
+    "April": "",
+    "May": "",
+    "June": "",
+    "July": "",
+    "August": "",
+    "September": "",
+    "October": "",
+    "November": "",
+    "December": "",
+
+    "Jan": "",
+    "Feb": "",
+    "Mar": "",
+    "Apr": "",
+    "Jun": "",
+    "Jul": "",
+    "Aug": "",
+    "Sep": "",
+    "Oct": "",
+    "Nov": "",
+    "Dec": ""
+};
